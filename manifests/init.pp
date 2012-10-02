ceb7258b0abac475577ff3984f81c5347b19a991
Lee Boynton <lee@lboynton.com>
2012-10-02 16:55:53 +0100

    Add hasstatus and hasrestart options. Restart redis when config file changes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,12 +4,13 @@ class redis(
     $maxmemory_policy = 'volatile-lru',
 ) {
     package { 'redis':
-        ensure => installed,
-        require => [
-            Class['epel'],
-        ]
+        ensure     => installed,
+        require    => Class['epel'],
+        hasstatus  => true,
+        hasrestart => true,
     }
     file { '/etc/redis.conf':
         content => template('redis/redis.conf.erb'),
+        notify  => Service['redis'],
     }
 }
