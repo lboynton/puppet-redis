5540f4f4baf1902120645c49fc056e83438c95b2
Lee Boynton <lee@lboynton.com>
2012-10-02 16:51:58 +0100

    Allow databases, max memory and max memory policy to be configured with redis class.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,15 @@
-class redis {
+class redis(
+    $databases        = 16,
+    $maxmemory        = undef,
+    $maxmemory-policy = 'volatile-lru',
+) {
     package { 'redis':
         ensure => installed,
         require => [
             Class['epel'],
         ]
+    }
+    file { '/etc/redis.conf':
+        content => template('redis/redis.conf.erb'),
     }
 }
